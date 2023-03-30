2089910e73fae2fa172a29e9025dfda49dd02101
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-03-30 11:41:16 -0300

    feat: load envs path

---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+
+export function loadEnv() {
+  const path =
+    process.env.NODE_ENV === "test"
+      ? ".env.test"
+      : process.env.NODE_ENV === "development"
+      ? ".env.dev"
+      : process.env.NODE_ENV === "prod"
+      ? ".env.prod"
+      : ".env";
+
+  const currentEnvs = dotenv.config({ path });
+  dotenvExpand.expand(currentEnvs);
+}

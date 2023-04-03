c3fd09d63f44c361dbf7b0f2f07332a535ad7b0a
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-03 09:31:27 -0300

    feat: add new repository functions to get products by id and by category

--- src/repositories/product-repository.ts
+++ src/repositories/product-repository.ts
@@ -1,6 +1,7 @@
 import { database } from "@/config";
 import { Product } from "@/models";
 import { CreateProductParams } from "@/schemas/protocols";
+import { ObjectId } from "mongodb";
 
 async function mongoInsertProduct(data: CreateProductParams) {
   return await database.collection("products").insertOne(data);
@@ -14,10 +15,21 @@ async function mongoFindProducts(): Promise<Product[]> {
   return await database.collection<Product>("products").find().toArray();
 }
 
+async function mongoFindProductsOfCategory(category: string): Promise<Product[]> {
+  return await database.collection<Product>("products").find({ category }).toArray();
+}
+
+async function mongoFindProductById(productId: string) {
+  const _id = new ObjectId(productId);
+  return await database.collection<Omit<Product, "_id">>("products").findOne({ _id });
+}
+
 const productRepository = {
   mongoInsertProduct,
   mongoFindProductByName,
   mongoFindProducts,
+  mongoFindProductsOfCategory,
+  mongoFindProductById,
 };
 
 export { productRepository };
